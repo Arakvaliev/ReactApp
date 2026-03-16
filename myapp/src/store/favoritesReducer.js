085e1ffb20c90e293eaf017e93d5935d15de72be
Arakvaliev <senkaval@gmail.com>
2026-03-16 11:48:15 +0300

    Added loading from local storage

--- myapp/src/store/favoritesReducer.js
+++ myapp/src/store/favoritesReducer.js
@@ -1,17 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("favorites");
+    if (serializedState === null) {
+      return [];
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error("Ошибка загрузки из localStorage:", err);
+    return [];
+  }
+};
 
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("favorites", serializedState);
+  } catch (err) {
+    console.error("Ошибка сохранения в localStorage:", err);
+  }
+};
+
+const initialState = loadState();
 export const FavoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
     addToFavorites: (state, action) => {
       state.push(action.payload);
+      saveState(state);
     },
     removeFromFavorites: (state, action) => {
-      const data = state.filter((el) => el.id !== action.payload);
-      return [...data];
+      const newState = state.filter((el) => el.id !== action.payload);
+      saveState(newState);
+      return newState;
     },
   },
 });
